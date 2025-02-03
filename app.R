2d5f8acdd6512779bd9dc20aee3bd1faa18e9d49
sagesteppe <reedbenkendorf27@gmail.com>
2025-02-03 12:51:39 -0600

    slider example

--- app.R
+++ app.R
@@ -1,16 +1,6 @@
 # remotes::install_github("surveydown-dev/surveydown", force = TRUE)
 library(surveydown)
 
-# Database setup
-
-# surveydown stores data on a database that you define at https://supabase.com/
-# To connect to a database, update the sd_database() function with details
-# from your supabase database. For this demo, we set ignore = TRUE, which will
-# ignore the settings and won't attempt to connect to the database. This is
-# helpful for local testing if you don't want to record testing data in the
-# database table. See the documentation for details:
-# https://surveydown.org/store-data
-
 db <- sd_database(
   host   = "",
   dbname = "",
@@ -23,10 +13,6 @@ db <- sd_database(
 
 # Server setup
 server <- function(input, output, session) {
-
-  # Define any conditional skip logic here (skip to page if a condition is true)
-  sd_skip_if(
-    input$respondent_FC_region == 11 ~ "Alaska_lennon_birth" )
 
   # Define any conditional display logic here (show a question if a condition is true)
 
